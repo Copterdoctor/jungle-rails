3d9e9eeb37cd7dc0bb1da34103bf4f9472264081
Copterdoctor <jordananders@me.com>
2018-07-23 23:05:17 +0000

    user tests added

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,17 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :reviews
   
-  validates_uniqueness_of :email
+  validates :email, uniqueness: { case_sensitive: false }
+  validates :email, presence: true
   validates :first_name, presence: true
   validates :last_name, presence: true
+  validates :password, length: { minimum: 8 }
 
+  def self.authenticate_with_credentials(user, email, password)
+    if (!!(email =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i)) && user.authenticate(password)
+      true
+    else
+      false
+    end
+  end
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,84 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+
+  describe 'Validations' do
+    it "Creates a new user with valid info" do
+      user = User.new(:first_name => "TestName", :last_name => "LastName", :email => "example@example.com", :password => "password", :password_confirmation => "password")
+      expect(user).to be_valid
+      expect(user.errors.full_messages).to eq([])
+    end
+
+    it "Does not create a new user with no first name" do
+      user = User.new(:first_name => "", :last_name => "LastName", :email => "example@example.com", :password => "password", :password_confirmation => "password")
+      expect(user).to_not be_valid
+      expect(user.errors.full_messages).to eq(["First name can't be blank"])
+    end
+
+    it "Does not create a new user with no last name" do
+      user = User.new(:first_name => "TestName", :last_name => "", :email => "example@example.com", :password => "password", :password_confirmation => "password")
+      expect(user).to_not be_valid
+      expect(user.errors.full_messages).to eq(["Last name can't be blank"])
+    end
+
+    it "Does not create a new user with no email" do
+      user = User.new(:first_name => "TestName", :last_name => "LastName", :email => nil, :password => "password", :password_confirmation => "password")
+      expect(user).to_not be_valid
+      expect(user.errors.full_messages).to eq(["Email can't be blank"])
+    end
+
+    it "Does not create a new user with no password" do
+      user = User.new(:first_name => "TestName", :last_name => "LastName", :email => "example@example.com", :password => "", :password_confirmation => "password")
+      expect(user).to_not be_valid
+      expect(user.errors.full_messages).to eq(["Password can't be blank", "Password is too short (minimum is 8 characters)"])
+    end
+
+    it "Does not create a new user with no password confirmation" do
+      user = User.new(:first_name => "TestName", :last_name => "LastName", :email => "example@example.com", :password => "password", :password_confirmation => "")
+      expect(user).to_not be_valid
+      expect(user.errors.full_messages).to eq(["Password confirmation doesn't match Password"])
+    end
+
+    it "Does not create a new user with password less than 8 char" do
+      user = User.new(:first_name => "TestName", :last_name => "LastName", :email => "example@example.com", :password => "passwor", :password_confirmation => "passwor")
+      expect(user).to_not be_valid
+      expect(user.errors.full_messages).to eq(["Password is too short (minimum is 8 characters)"])
+    end
+
+    it "Does not create a new user with email already in db" do
+      user = User.create(:first_name => "TestName", :last_name => "LastName", :email => "example@example.com", :password => "password", :password_confirmation => "password")
+      user2 = User.create(:first_name => "TestName", :last_name => "LastName", :email => "example@example.com", :password => "password", :password_confirmation => "password")
+      expect(user).to be_valid
+      expect(user.errors.full_messages).to eq([])
+      expect(user2).to_not be_valid
+      expect(user2.errors.full_messages).to eq(["Email has already been taken"])
+    end
+
+    
+  end
+
+  describe '.authenticate_with_credentials' do
+    it "Does authentic user" do
+      user = User.create(:first_name => "TestName", :last_name => "LastName", :email => "example@example.com", :password => "password", :password_confirmation => "password")
+      expect(User.authenticate_with_credentials(user, user[:email], "password")).to be true
+    end
+
+    it "Does not authentic user without password" do
+      user = User.create(:first_name => "TestName", :last_name => "LastName", :email => "example@example.com", :password => "password", :password_confirmation => "password")
+      expect(User.authenticate_with_credentials(user, user[:email], nil)).to be false
+    end
+
+    it "Does not authentic user with wrong password" do
+      user = User.create(:first_name => "TestName", :last_name => "LastName", :email => "example@example.com", :password => "password", :password_confirmation => "password")
+      expect(User.authenticate_with_credentials(user, user[:email], "nil")).to be false
+    end
+
+    it "Does not authentic user without email" do
+      user = User.create(:first_name => "TestName", :last_name => "LastName", :email => "example@example.com", :password => "password", :password_confirmation => "password")
+      expect(User.authenticate_with_credentials(user, nil, "password")).to be false
+    end
+
+  end
+
+end
+
